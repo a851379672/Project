486b217d8dd9863d3e05e5faf8bb2ba069648514
runtao_ <57519143+a851379672@users.noreply.github.com>
2022-07-06 18:02:26 +0800

    Add files via upload
    update return

--- httprunner3/base/base_file.py
+++ httprunner3/base/base_file.py
@@ -1,22 +1,29 @@
 import os.path
 import re
-from settings import *
+from common.settings import *
+import datetime
 
 
 def base_testfile():
     """
-    文件格式：课程视频_0.mp4
-    :return: file_name: 课程视频_1.mp4
+    文件格式：课程视频.mp4
+    :return: file_name: 课程视频 2022-06-01 19:53:13.mp4
     """
-    folder = [os.path.join(paths, filename) for paths, files, name in os.walk(FILE_PATH) for filename in files]
+    folder = [os.path.join(path_, filename) for path_, files, name_ in os.walk(FILE_PATH) for filename in files]
     file_name = [name for folder in folder for path, file, name in os.walk(folder) if name]
     num = 0
+    date_time = datetime.datetime.now().strftime("%Y-%m-%d %H：%M：%S")
     for file_list in file_name:
         for file in file_list:
-            old_name, format_ = os.path.splitext(file)
-            file_num = int(re.findall(r'_(\d+)+', f'{old_name}')[0])
-            re_name = old_name.replace(str(file_num), str(file_num + 1))
-            os.rename(fr'{folder[num]}\{old_name + format_}', fr'{folder[num]}\{re_name + format_}')
+            old_name, file_type = os.path.splitext(file)
+            raw_file = os.path.join(folder[num], old_name + file_type)
+            try:
+                new_file = os.path.join(folder[num], old_name.replace(
+                    re.findall(r' (.*)', old_name)[0], date_time + file_type)
+                                        )
+            except IndexError:
+                new_file = os.path.join(folder[num], old_name + ' ' + date_time + file_type)
+            os.rename(raw_file, new_file)
         num += 1
 
 
